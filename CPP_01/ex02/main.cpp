a500a5f69a8a47f9210f28ab5e04aefcecec667f
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2024-11-15 15:07:13 +0100

    ex02

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarzouk <rmarzouk@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 18:46:00 by rmarzouk          #+#    #+#             */
+/*   Updated: 2024/11/06 18:53:01 by rmarzouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+	
+	std::cout << &str << std::endl;
+	std::cout << &stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	std::cout << str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
