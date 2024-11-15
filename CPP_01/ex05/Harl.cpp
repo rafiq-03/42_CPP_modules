1aaa9ee581bf0d26683ad0c587b54c55c64390ce
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2024-11-15 15:08:42 +0100

    ex05

---
+++ CPP_01/ex05/Harl.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarzouk <rmarzouk@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 16:55:26 by rmarzouk          #+#    #+#             */
+/*   Updated: 2024/11/08 17:49:14 by rmarzouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void Harl::complain( std::string level )
+{
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void	(Harl::*funcptr[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->*funcptr[i])();
+			return ;
+		}	
+	}
+}
+
+void Harl::debug( void )
+{
+	std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+void Harl::info( void )
+{
+	std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+void Harl::warning( void )
+{
+	std::cout << "[WARNING]]\nI think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month."	<< std::endl;
+}
+void Harl::error( void )
+{
+	std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now." << std::endl;
+}
