8b78f4a389e13bca405f4e8e732d75e1b07cbb74
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2024-11-16 10:13:42 +0100

    ex01

---
+++ CPP_02/ex01/Fixed.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarzouk <rmarzouk@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 12:49:22 by rmarzouk          #+#    #+#             */
+/*   Updated: 2024/11/12 16:50:06 by rmarzouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class	Fixed
+{
+	private:
+		int	_value;
+		static const int _fractionalBits;
+	public:
+		Fixed();
+		Fixed(const int value);// ex01;
+		Fixed(const float value);// ex01;
+		Fixed(const Fixed &other);
+		~Fixed();
+		Fixed& operator=(const Fixed &other);
+		int	getRawBits( void ) const;
+		void setRawBits( int const raw );
+		float toFloat( void ) const;// ex01
+		int toInt( void ) const;// ex01
+		friend std::ostream& operator<<(std::ostream &out, const Fixed &a);
+};
+
+#endif
---
+++ CPP_02/ex01/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarzouk <rmarzouk@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 15:29:18 by rmarzouk          #+#    #+#             */
+/*   Updated: 2024/11/12 16:51:08 by rmarzouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp" 
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return 0;
+}
