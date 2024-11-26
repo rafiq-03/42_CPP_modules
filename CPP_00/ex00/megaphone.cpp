60e2aa530b72c1345e3c764e148a03470489c670
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2024-11-26 13:56:57 +0100

    done ✅

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rmarzouk <rmarzouk@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 12:56:02 by rmarzouk          #+#    #+#             */
-/*   Updated: 2024/11/25 11:11:52 by rmarzouk         ###   ########.fr       */
+/*   Updated: 2024/11/26 13:56:40 by rmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main(int ac, char **av)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	while (k < ac)
 	{
-		 i = 0;
+		i = 0;
 		while (av[k] && av[k][i])
 			std::cout << (char)toupper(av[k][i++]);
 		k++;
