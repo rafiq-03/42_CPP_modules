95341b1489a2a0b04b58347ef2ff95b5fda51de1
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2025-03-12 17:21:00 +0000

    handle case of array is null

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: rmarzouk <rmarzouk@student.1337.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:36:41 by rmarzouk          #+#    #+#             */
-/*   Updated: 2025/02/06 16:52:20 by rmarzouk         ###   ########.fr       */
+/*   Updated: 2025/03/11 14:53:40 by rmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 #include <iostream>
 
-template <typename T> void iter(T *array, size_t length, void func(T &element)){
+template <typename T>
+void iter(T *array, size_t length, void func(T &element)){//null check !!
+    if (!array)
+        return;
     for (size_t i = 0; i < length; i++)
         func(array[i]);
 }
