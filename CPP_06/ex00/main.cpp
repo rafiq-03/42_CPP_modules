30f779479516694fc86837f38b49ae9e414999db
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2025-03-18 11:41:18 +0000

    some changes in main

--- CPP_06/ex00/main.cpp
+++ CPP_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmarzouk <rmarzouk@student.1337.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 09:29:55 by rmarzouk          #+#    #+#             */
-/*   Updated: 2025/02/06 15:28:51 by rmarzouk         ###   ########.fr       */
+/*   Updated: 2025/03/18 11:40:53 by rmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int main(int ac, char **av)
 {
-    if (ac != 2)
+    if (ac != 2){
+        std::cerr << "Usage: ./convert <value>" << std::endl;
         return (1);
+    }
     std::string input = av[1];
     ScalarConverter::convert(input);
     return (0);
