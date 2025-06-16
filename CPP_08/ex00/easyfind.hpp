50d7a84d813a52482778884d2adbb11f18b1ee2b
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2025-06-16 17:05:48 +0100

    fix some buges in ex00

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: rmarzouk <rmarzouk@student.1337.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 18:54:18 by rmarzouk          #+#    #+#             */
-/*   Updated: 2025/04/19 20:07:40 by rmarzouk         ###   ########.fr       */
+/*   Updated: 2025/06/16 15:29:24 by rmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 template<typename T>
 typename T::iterator easyfind(T& container, int target)
 {
-    for(typename T::iterator it(container.begin()); it != container.end(); it++){
+    for(typename T::iterator it = container.begin(); it != container.end(); it++){
         if (*it == target)
             return (it);
     }
-    throw std::runtime_error("not found\n");
+    throw std::runtime_error("Not Found");
 }
 
 #endif
--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmarzouk <rmarzouk@student.1337.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 18:04:23 by rmarzouk          #+#    #+#             */
-/*   Updated: 2025/04/19 20:15:39 by rmarzouk         ###   ########.fr       */
+/*   Updated: 2025/06/16 16:25:59 by rmarzouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int main(void)
         std::list<int> list(12);
         std::srand(std::time(NULL));
         for (std::list<int>::iterator it = list.begin(); it != list.end(); it++)
-            *it = std::rand() % 20;
+            *it = std::rand() % 200;
         std::list<int>::iterator a = easyfind(list, 11);
-        std::cout << "found number " << *a << std::endl;
+        std::cout << "Found Number: " << *a << std::endl;
     }
     catch(const std::exception& e)
     {
