668eef1668777c04c737b23c2b7bc8dd3c60187d
Rafiq Marzouk <rafiq.marzouk@gmail.com>
2025-08-20 19:26:46 +0100

    another save for this day

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -56,7 +56,6 @@ void	PmergeMe::swapPairs(std::deque<int> &chain, size_t step, std::deque<int>::i
 		*(it + step) = tmp;
 	}
 	static_cast<void>(chain);
-	static_cast<void>(bgn);
 }
 
 int pow(int nb, int pow){
@@ -68,28 +67,31 @@ int pow(int nb, int pow){
 	return res;
 }
 
-// void	PmergeMe::insertion(std::deque<int> &chain, size_t step, size_t ref, int nb){
-// 	// size_t begin = 0;
-// 	// size_t end = ref;
-// 	// size_t middle = (end - begin) / 2;
+void	PmergeMe::insertion(std::deque<int> &chain, size_t step, size_t ref, int nb){
+	size_t begin = step - 1;
+	size_t end = ref;
 
-// 	std::lower_bound(chain.begin(), chain.begin() + ref, nb);	
-// 	// implement binary search
-// 	// try to insert a range of element from pend to this position
+	// implement binary search
+	// try to insert a range of element from pend to this position
+	while (begin < end){
+		size_t middle = (end - begin) / 2;
+		if (chain[middle] > nb)// comp
+			end = middle;
+		else if (chain[middle] > nb)
+			begin = middle;
+	}
+	
+}
 
-// }
 
-
-void	PmergeMe::mergeSort(std::deque<int> &arr, size_t begin, size_t lvl){
+void	PmergeMe::mergeSort(std::deque<int> &arr,size_t lvl){
 	size_t step = pow(2, lvl);
 	if (step > arr.size()){
 		return;
 	}
-	std::deque<int> pend;
-	// std::cout << "level : " << lvl  << " step = " << step << std::endl;
-	for (size_t i = 0; i < arr.size(); i += step)
+	std::cout << "level : " << lvl  << " step = " << step << std::endl << "          ";
+	for (size_t i = 0; i < arr.size(); i += step)// loop over array and swap bigger with smaller numbers
 	{
-		// here we should check the biggest pair and swap it
 		if (i + step > arr.size()){
 			std::cout << "\nleft elemnts : [ ";
 			for (size_t j = i; j < arr.size(); j++)
@@ -105,32 +107,52 @@ void	PmergeMe::mergeSort(std::deque<int> &arr, size_t begin, size_t lvl){
 		// }
 		// std::cout << "] " ;
 
-
-
-
 		if (arr[i + step / 2 - 1] > arr[i + step -1])
 			swapPairs(arr, step / 2, arr.begin() + i);
-		// std::cout << "[";
-		// earase form arr and push it in specific order
-		
 
 		for (size_t j = i; j < i + step; j++){
-			if (j == i + step / 2)
-				std::cout << ".";
+			// if (j == i + step / 2)
+			// 	std::cout << ".";
 			std::cout << (j < arr.size() ? arr[j] : -1 ) << " ";
 		}
-		std::cout << "| " ;
+		// std::cout << "| " ;
 	}
+
 	std::cout << std::endl;
-	mergeSort(arr, begin, lvl + 1);
+	// create main chain and pend chain;
+	std::deque<int> main;
+	std::deque<int> pend;
+	size_t i;
+	for (i = 0; i < arr.size(); i += step){
+		if (i + step / 2 <= arr.size())
+			pend.insert(pend.end(), arr.begin() + i, arr.begin() + i + step / 2);
+		if (i + step <= arr.size())
+			main.insert(main.end(), arr.begin() + i + step / 2, arr.begin() + i + step);
+	}
+	for (size_t j = i; j < arr.size(); j++)
+		main.push_back(arr[j]);
+
+	// std::cout << main.size() << " main : "  ;
+	// for (size_t i = 0; i < main.size(); i++)
+	// 	std::cout << "   " <<  main[i]  << " ";
+	// std::cout << std::endl;
+	// std::cout << pend.size() << " pend : " ;
+	// for (size_t i = 0; i < pend.size(); i++)
+	// 	std::cout <<  pend[i]  << "    ";
+	// std::cout << "\n"<< std::endl;
+	
+	// binary search and insertion of numbers from pend to main;
+
+	arr = main;
+	mergeSort(arr, lvl + 1);
+}
+
 	// how to check if we can't make another pair ?
 	// how remove a specific element (pair) from a place and insert it in another place?
 	// pend chain
 	// main chain
 	// how to keep track pairs based on level?
 	// what is the base condition for this methode?
-}
-
 void	PmergeMe::fordJohnsonSort(std::deque<int> &Chain, std::deque<int>::iterator bgn){
 	// make pairs;
 	// swap the biggest ones with smallest ones [small, big];
@@ -139,6 +161,7 @@ void	PmergeMe::fordJohnsonSort(std::deque<int> &Chain, std::deque<int>::iterator
 	// make main chain like [s1, b1, b2, ..... bn ] and pend chain what is left for smalls [s2, s3, ..... sn]
 	// now we have every sn < bn so we should insert every sn from pend chain in [s1, b1, .... bn]. don't forget to use order based on jacobsthal order
 	static size_t level = 1;
-	mergeSort(Chain, 0, level);
+	std::cout << "size : " << Chain.size() << std::endl;
+	mergeSort(Chain,level);
 	(void)bgn;
 }
--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -19,7 +19,8 @@ class PmergeMe
 		~PmergeMe();
 		void	swapPairs(std::deque<int> &chain, size_t step, std::deque<int>::iterator bgn);
 		void	fordJohnsonSort(std::deque<int> &Chain, std::deque<int>::iterator bgn);
-		void	mergeSort(std::deque<int> &arr, size_t begin, size_t lvl);
+		void	mergeSort(std::deque<int> &arr, size_t lvl);
+		void	insertion(std::deque<int> &chain, size_t step, size_t ref, int nb);
 };
 int pow(int nb, int pow);
 #endif
